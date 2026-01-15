25641f3d00e4ea76f11553edb7572e9124e9a3a6
sangeetha arun <sangeetha.arun@space01.co.uk>
2026-01-15 14:05:57 +0000

    Secure API with cognito pool authorizer

--- cdk/lib/stacks/api-stack.ts
+++ cdk/lib/stacks/api-stack.ts
@@ -3,13 +3,18 @@ import { Construct } from "constructs";
 import * as apigw from "aws-cdk-lib/aws-apigateway";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as cognito from "aws-cdk-lib/aws-cognito";
+
+export interface ApiStackProps extends cdk.StackProps {
+  userPool: cognito.IUserPool;
+}
 
 export class ApiStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: ApiStackProps) {
     super(scope, id, props);
 
     const handler = new NodejsFunction(this, "ItemsHandler", {
-      runtime: lambda.Runtime.NODEJS_18_X,
+      runtime: lambda.Runtime.NODEJS_22_X,
       entry: "lambda/items-handler/index.ts",
       handler: "proxyHandler",
     });
@@ -18,13 +23,36 @@ export class ApiStack extends cdk.Stack {
       restApiName: "kids-school-tracker-api",
     });
 
+    const authorizer = new apigw.CognitoUserPoolsAuthorizer(
+      this,
+      "SchoolTrackerAuthorizer",
+      {
+        cognitoUserPools: [props!.userPool],
+      }
+    );
+
     const items = api.root.addResource("items");
-    items.addMethod("GET", new apigw.LambdaIntegration(handler));
-    items.addMethod("POST", new apigw.LambdaIntegration(handler));
+    items.addMethod("GET", new apigw.LambdaIntegration(handler), {
+      authorizer,
+      authorizationType: apigw.AuthorizationType.COGNITO,
+    });
+    items.addMethod("POST", new apigw.LambdaIntegration(handler), {
+      authorizer,
+      authorizationType: apigw.AuthorizationType.COGNITO,
+    });
 
     const item = items.addResource("{id}");
-    item.addMethod("GET", new apigw.LambdaIntegration(handler));
-    item.addMethod("PUT", new apigw.LambdaIntegration(handler));
-    item.addMethod("DELETE", new apigw.LambdaIntegration(handler));
+    item.addMethod("GET", new apigw.LambdaIntegration(handler), {
+      authorizer,
+      authorizationType: apigw.AuthorizationType.COGNITO,
+    });
+    item.addMethod("PUT", new apigw.LambdaIntegration(handler), {
+      authorizer,
+      authorizationType: apigw.AuthorizationType.COGNITO,
+    });
+    item.addMethod("DELETE", new apigw.LambdaIntegration(handler), {
+      authorizer,
+      authorizationType: apigw.AuthorizationType.COGNITO,
+    });
   }
 }
---
+++ cdk/lib/stacks/auth-stack.ts
@@ -0,0 +1,43 @@
+import { Stack, StackProps, CfnOutput, RemovalPolicy } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as cognito from "aws-cdk-lib/aws-cognito";
+
+export class AuthStack extends Stack {
+  public readonly userPool: cognito.UserPool;
+  public readonly userPoolClient: cognito.UserPoolClient;
+
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    this.userPool = new cognito.UserPool(this, "SchoolTrackerUserPool", {
+      userPoolName: "kids-school-tracker-user-pool",
+      selfSignUpEnabled: false,
+      signInAliases: { email: true },
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+      passwordPolicy: {
+        minLength: 8,
+        requireDigits: true,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireSymbols: false,
+      },
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    this.userPoolClient = this.userPool.addClient(
+      "SchoolTrackerUserPoolClient",
+      {
+        userPoolClientName: "kids-school-tracker-user-pool-client",
+        authFlows: {
+          userPassword: true,
+        },
+        generateSecret: false,
+      }
+    );
+
+    new CfnOutput(this, "UserPoolId", { value: this.userPool.userPoolId });
+    new CfnOutput(this, "UserPoolClientId", {
+      value: this.userPoolClient.userPoolClientId,
+    });
+  }
+}
