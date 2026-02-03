c742558ed143492e9a55759c76ad344e8ab93aaa
sangeetha arun <sangeetha.arun@space01.co.uk>
2026-02-03 11:07:14 +0000

    Updated pipeline stack

--- cdk/lib/stacks/pipeline-stack.ts
+++ cdk/lib/stacks/pipeline-stack.ts
@@ -34,7 +34,7 @@ export class PipelineStack extends cdk.Stack {
           // CDK synth steps
           "cd cdk",
           "npm ci",
-          "npx cdk synth -o ../cdk.out",
+          "npx cdk synth",
         ],
         primaryOutputDirectory: "cdk/cdk.out",
       }),
